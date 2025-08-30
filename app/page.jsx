879e579eb810e8d5907fad784d76ecffee6bfd66
Zahid Ahmed <zahd87956@gmail.com>
2025-08-30 18:32:24 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -10,10 +10,8 @@ import Footer from "@/components/Footer";
 const Home = () => {
   return (
     <>
-      <Navbar/>
       <div className="px-6 md:px-16 lg:px-32">
     <h1>WEBSITE HAS FATAL ERROR</h1>      </div>
-      <Footer />
     </>
   );
 };
